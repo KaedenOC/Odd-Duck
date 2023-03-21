7c6e4bee8771513d5292715a6908eb12b6ee59ec
KaedenOC <komeacarr2@gmail.com>
2023-03-20 18:23:33 -0700

    pathing change

--- js/app.js
+++ js/app.js
@@ -11,25 +11,25 @@ function Image (name, source) {
     this.timesClicked = 0;
 }
 
-state.push(new Image('bag', '/img/bag.jpg'));
-state.push(new Image('banana', '/img/banana.jpg'));
-state.push(new Image('bathroom', '/img/bathroom.jpg'));
-state.push(new Image('boots', '/img/boots.jpg'));
-state.push(new Image('breakfast', '/img/breakfast.jpg'));
-state.push(new Image('bubblegum', '/img/bubblegum.jpg'));
-state.push(new Image('chair', '/img/chair.jpg'));
-state.push(new Image('cthulhu', '/img/cthulhu.jpg'));
-state.push(new Image('dog-duck', '/img/dog-duck.jpg'));
-state.push(new Image('dragon', '/img/dragon.jpg'));
-state.push(new Image('pen', '/img/pen.jpg'));
-state.push(new Image('pet-sweep', '/img/pet-sweep.jpg'));
-state.push(new Image('scissors', '/img/scissors.jpg'));
-state.push(new Image('shark', '/img/shark.jpg'));
-state.push(new Image('sweep', '/img/sweep.png'));
-state.push(new Image('tauntaun', '/img/tauntaun.jpg'));
-state.push(new Image('unicorn', '/img/unicorn.jpg'));
-state.push(new Image('water-can', '/img/water-can.jpg'));
-state.push(new Image('wine-glass', '/img/wine-glass.jpg'));
+state.push(new Image('bag', './img/bag.jpg'));
+state.push(new Image('banana', './img/banana.jpg'));
+state.push(new Image('bathroom', './img/bathroom.jpg'));
+state.push(new Image('boots', './img/boots.jpg'));
+state.push(new Image('breakfast', './img/breakfast.jpg'));
+state.push(new Image('bubblegum', './img/bubblegum.jpg'));
+state.push(new Image('chair', './img/chair.jpg'));
+state.push(new Image('cthulhu', './img/cthulhu.jpg'));
+state.push(new Image('dog-duck', './img/dog-duck.jpg'));
+state.push(new Image('dragon', './img/dragon.jpg'));
+state.push(new Image('pen', './img/pen.jpg'));
+state.push(new Image('pet-sweep', './img/pet-sweep.jpg'));
+state.push(new Image('scissors', './img/scissors.jpg'));
+state.push(new Image('shark', './img/shark.jpg'));
+state.push(new Image('sweep', './img/sweep.png'));
+state.push(new Image('tauntaun', './img/tauntaun.jpg'));
+state.push(new Image('unicorn', './img/unicorn.jpg'));
+state.push(new Image('water-can', './img/water-can.jpg'));
+state.push(new Image('wine-glass', './img/wine-glass.jpg'));
 //these are pushed to the front of the array
 
 let imgEls = document.querySelectorAll("img"); //this will grab all the images
